be9a960aa5991c0ab7ab088fd846e3097fcafc3a
Carly Harris <carlyrharris26@gmail.com>
2016-09-26 00:03:14 +0000

    Add play_again

--- rps.rb
+++ rps.rb
@@ -1,21 +1,32 @@
-puts "Would you like to play? (yes/no)"
-input =gets.chomp.downcase
-if input == "yes"
+def play_again?
+   puts "Would you like to play? (yes/no)"
+    input = gets.chomp.downcase
+
+    if input == "yes"
+      play_game
+    elsif input == "no"
+      puts "See you next time!"
+    end 
+end
+
+def play_game
     puts "Let's begin!"
     puts "starting game..."
     puts "Press 'r' for rock, 'p' for paper, and 's' for scissors."
-    
-    user_input =gets.chomp.downcase
+
+    user_input = gets.chomp.downcase
     computer_input = ["r", "p", "s"].sample
-    
+
     case [user_input, computer_input]
-    when ["r","s"], ["s", "p"], ["p","r"]
+    when["r", "s"], ["s", "p"], ["p", "r"]
         puts "You won! COngrats!"
-    when ["r", "r"], ["p", "p"], ["s", "s"]
+    when["r", "r"], ["p", "p"], ["s", "s"]
         puts "You tied!"
     else
         puts "You lost, sorry."
     end
-elsif input == "no"
-    puts "See you next time!"
+     puts "The computer chose #{computer_input}."
+     play_again?
 end
+
+play_again?
