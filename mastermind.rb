6092d59fee68185a2dfebb30a1c2bcb3b7fc374e
Carly Harris <carlyrharris26@gmail.com>
2016-11-14 01:00:26 +0000

    FINALLY!

--- mastermind.rb
+++ mastermind.rb
@@ -1,9 +1,11 @@
+secret_code = nil
+
 def start_up
  puts "Wanna play? (yes/no)"
  input = gets.chomp.downcase
  if input == "yes"
      puts "Cool! Let's start!"
-     new_game
+     loop
  elsif input == "no"
      puts "See ya soon!"
  else
@@ -11,20 +13,31 @@ def start_up
  end
 end
 
-def new_game
-    secret_code = generate_secret_code
+def loop
+ secret_code = generate_secret_code
+ puts secret_code
+ new_game(secret_code)
+end
+
+def new_game(secret_code)
     puts "Loading..."
     puts "The goal of the game is for your input to match the computer's selection. Keep trying until you get it."
     puts "Enter four letters out of r (red), g (green), b (blue), and y (yellow). You may put multiple of the same Letter."
     
+    next_round(secret_code)
+end
+    
+def next_round(secret_code)
     user_input = gets.chomp.downcase.chars
     
     if user_input == secret_code
-        puts "ACHIEVEMENT UNLOCKED: Boss Status! You got it right!"
+        puts "ACHIEVEMENT UNLOCKED: Boss Status! The correct combination was #{secret_code}!"
+        start_up
     else
         num_colors_matching = numbers_matching(user_input, secret_code)
         num_colors_right_position = numbers_matching_position(user_input, secret_code)
         puts "You have #{num_colors_matching} colors matching, #{num_colors_right_position} in the right position. "
+        next_round(secret_code)
     end
 end
 
@@ -38,9 +51,8 @@ def generate_secret_code
 end
 
 def numbers_matching(guess, code)
-    guess.uniq!
     count = 0
-    guess.each do |color|
+    guess.uniq.each do |color|
         if code.include?(color)
             count += 1 
         end 
@@ -57,6 +69,7 @@ def numbers_matching_position(guess, code)
      end
      count
 end
+
 
 
 start_up
